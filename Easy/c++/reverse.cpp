3939c80506cfe44b5ca90056bc90c51ff50f5c15
Asuman Aydın <asuman.aydin6709@gmail.com>
2021-09-06 20:19:04 +0300

    reverse

--- Easy/c++/reverse.cpp
+++ Easy/c++/reverse.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int reverse(int n) {
-     
-      int result = 0;
-      while(n != 0)
-      {
-          int tail = n%10;
-          int newResult = result*10 + tail;
-          result = newResult;
-          n = n/10;
-      }
-      return result;
+    int reverse(int x) {
+        long result = 0;
+    while(x != 0)
+    {
+        result = result*10 + x % 10;
+        x /= 10;
+    }
+    return (result > INT_MAX || result < INT_MIN)? 0 : result;
+        
     }
 };
